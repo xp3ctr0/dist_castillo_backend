f4b4f9d32f236f27d3839e39d20775e92d305d7b
Jhon Edinson Gonzalez Cortes <jhegonza@bancolombia.com.co>
2023-03-13 20:11:56 -0500

    new change

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-import "dotenv/config";
+// import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import {router} from "./routes";
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 const app = express();
 app.use(cors());
 app.use(express.json());
